e70c62ea29390951509a0b45e5bffbaf3e08f53c
Tilde1999 <113102307+Tilde1999@users.noreply.github.com>
2022-09-15 11:08:19 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,47 +1,120 @@
-function plot (x: number, y: number) {
-    led.plot(x, y)
+function Fallright () {
+    if (unlocked == 1) {
+        unlocked += 1
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # # . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . # # .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # #
+            . . . . .
+            `)
+        basic.clearScreen()
+        boxx = 4
+        boxy = 3
+        led.plot(boxx, boxy)
+        unlocked += 1
+    }
+    radio.sendValue("xTarget", 4)
+    radio.sendValue("yTarget", 3)
 }
 function position (x: number, y: number) {
     led.unplot(xPosition, yPosition)
     xPosition = x
     yPosition = y
 }
-radio.onReceivedString(function (receivedString) {
-    if (receivedString == "snake") {
-        mode = 1
-    }
+input.onGesture(Gesture.Shake, function () {
+    Falldown()
 })
+input.onGesture(Gesture.TiltRight, function () {
+    Fallright()
+})
+function Falldown () {
+    if (unlocked == 0) {
+        basic.showLeds(`
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . # . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . . .
+            . # . . .
+            . . . . .
+            `)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+        unlocked += 1
+    }
+}
 radio.onReceivedValue(function (name, value) {
     if (name == "xPos") {
         position(value, yPosition)
     } else if (name == "yPos") {
         position(xPosition, value)
-    } else if (name == "xPlot") {
-        xPlot = value
-    } else if (name == "yPlot") {
-        plot(xPlot, value)
-    } else if (name == "xUnPlot") {
-        xUnPlot = value
-    } else if (name == "yUnPlot") {
-        unplot(xUnPlot, value)
     } else if (false) {
     	
     }
+    if (name == "interact" && value == 1) {
+        basic.showNumber(3)
+        unlocked += 1
+        radio.sendValue("xTarget", -1)
+        radio.sendValue("yTarget", -1)
+    }
 })
-function unplot (x: number, y: number) {
-    led.unplot(x, y)
-}
-let xUnPlot = 0
-let xPlot = 0
-let mode = 0
+let boxy = 0
+let boxx = 0
+let unlocked = 0
 let yPosition = 0
 let xPosition = 0
-radio.setGroup(1)
+radio.setGroup(10)
 xPosition = 0
 yPosition = 0
-mode = 0
+unlocked = 0
 basic.forever(function () {
-    if (mode == 0) {
-        led.plot(xPosition, yPosition)
+    led.plot(xPosition, yPosition)
+    if (unlocked == 1) {
+        led.plot(1, 3)
+    }
+    if (unlocked == 3) {
+        led.plot(4, 3)
+    }
+    if (unlocked == 4 && (xPosition > 4 || yPosition > 4)) {
+        basic.showNumber(3)
     }
 })
